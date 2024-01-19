4edadc3460bbc1fbaecca610dfffcf496c5a53cb
Ransi De Silva <ransi@Ubuntu2.myguest.virtualbox.org>
2024-01-20 02:51:15 +1100

    deamon startup script

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+case "$1" in
+  start)
+    echo "starting aesdserver"
+    start-stop-daemon -S -n -x aesdsocket -a /usr/bin/aesdsocket -- -d
+    ;;
+  stop)
+    echo "stopping aesdserver"
+    start-stop-daemon -K -n aesdsocket
+    ;;
+  *)
+    echo "Usage $0 {start | stop}"
+  exit 1
+esac
+
+exit 0
